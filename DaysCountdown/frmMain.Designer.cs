0ffda77a2dac872dd580aae67ac402af4bdb542d
Edward <edward_song@yeah.net>
2017-04-01 10:02:59 +0800

    初步完成,wait modify progressbar

--- DaysCountdown/frmMain.Designer.cs
+++ DaysCountdown/frmMain.Designer.cs
@@ -28,20 +28,199 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.prbDays = new System.Windows.Forms.ProgressBar();
+            this.grbStatus = new System.Windows.Forms.GroupBox();
+            this.lblMinutes = new System.Windows.Forms.Label();
+            this.prbMinutes = new System.Windows.Forms.ProgressBar();
+            this.lblHours = new System.Windows.Forms.Label();
+            this.prbHours = new System.Windows.Forms.ProgressBar();
+            this.lblDays = new System.Windows.Forms.Label();
+            this.grbSetting = new System.Windows.Forms.GroupBox();
+            this.btnSetting = new System.Windows.Forms.Button();
+            this.dtpDueDay = new System.Windows.Forms.DateTimePicker();
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timerNow = new System.Windows.Forms.Timer(this.components);
+            this.grbStatus.SuspendLayout();
+            this.grbSetting.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // prbDays
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.prbDays.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.prbDays.Location = new System.Drawing.Point(82, 25);
+            this.prbDays.Name = "prbDays";
+            this.prbDays.Size = new System.Drawing.Size(632, 45);
+            this.prbDays.TabIndex = 0;
+            // 
+            // grbStatus
+            // 
+            this.grbStatus.BackColor = System.Drawing.Color.Black;
+            this.grbStatus.Controls.Add(this.lblMinutes);
+            this.grbStatus.Controls.Add(this.prbMinutes);
+            this.grbStatus.Controls.Add(this.lblHours);
+            this.grbStatus.Controls.Add(this.prbHours);
+            this.grbStatus.Controls.Add(this.lblDays);
+            this.grbStatus.Controls.Add(this.prbDays);
+            this.grbStatus.ForeColor = System.Drawing.Color.White;
+            this.grbStatus.Location = new System.Drawing.Point(12, 25);
+            this.grbStatus.Name = "grbStatus";
+            this.grbStatus.Size = new System.Drawing.Size(735, 185);
+            this.grbStatus.TabIndex = 1;
+            this.grbStatus.TabStop = false;
+            this.grbStatus.Text = "Countdown Status";
+            // 
+            // lblMinutes
+            // 
+            this.lblMinutes.AutoSize = true;
+            this.lblMinutes.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinutes.Location = new System.Drawing.Point(9, 140);
+            this.lblMinutes.Name = "lblMinutes";
+            this.lblMinutes.Size = new System.Drawing.Size(73, 23);
+            this.lblMinutes.TabIndex = 4;
+            this.lblMinutes.Text = "Minutes";
+            // 
+            // prbMinutes
+            // 
+            this.prbMinutes.Location = new System.Drawing.Point(82, 127);
+            this.prbMinutes.Name = "prbMinutes";
+            this.prbMinutes.Size = new System.Drawing.Size(632, 45);
+            this.prbMinutes.TabIndex = 2;
+            // 
+            // lblHours
+            // 
+            this.lblHours.AutoSize = true;
+            this.lblHours.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHours.Location = new System.Drawing.Point(13, 88);
+            this.lblHours.Name = "lblHours";
+            this.lblHours.Size = new System.Drawing.Size(57, 23);
+            this.lblHours.TabIndex = 3;
+            this.lblHours.Text = "Hours";
+            // 
+            // prbHours
+            // 
+            this.prbHours.Location = new System.Drawing.Point(82, 76);
+            this.prbHours.Name = "prbHours";
+            this.prbHours.Size = new System.Drawing.Size(632, 45);
+            this.prbHours.TabIndex = 1;
+            // 
+            // lblDays
+            // 
+            this.lblDays.AutoSize = true;
+            this.lblDays.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDays.Location = new System.Drawing.Point(21, 38);
+            this.lblDays.Name = "lblDays";
+            this.lblDays.Size = new System.Drawing.Size(48, 23);
+            this.lblDays.TabIndex = 2;
+            this.lblDays.Text = "Days";
+            // 
+            // grbSetting
+            // 
+            this.grbSetting.Controls.Add(this.label2);
+            this.grbSetting.Controls.Add(this.label1);
+            this.grbSetting.Controls.Add(this.dtpStart);
+            this.grbSetting.Controls.Add(this.btnSetting);
+            this.grbSetting.Controls.Add(this.dtpDueDay);
+            this.grbSetting.ForeColor = System.Drawing.Color.White;
+            this.grbSetting.Location = new System.Drawing.Point(770, 25);
+            this.grbSetting.Name = "grbSetting";
+            this.grbSetting.Size = new System.Drawing.Size(238, 185);
+            this.grbSetting.TabIndex = 2;
+            this.grbSetting.TabStop = false;
+            this.grbSetting.Text = "Setting";
+            // 
+            // btnSetting
+            // 
+            this.btnSetting.Font = new System.Drawing.Font("Calibri", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSetting.ForeColor = System.Drawing.Color.Black;
+            this.btnSetting.Location = new System.Drawing.Point(58, 128);
+            this.btnSetting.Name = "btnSetting";
+            this.btnSetting.Size = new System.Drawing.Size(132, 35);
+            this.btnSetting.TabIndex = 3;
+            this.btnSetting.Text = "Setting";
+            this.btnSetting.UseVisualStyleBackColor = true;
+            this.btnSetting.Click += new System.EventHandler(this.btnSetting_Click);
+            // 
+            // dtpDueDay
+            // 
+            this.dtpDueDay.Location = new System.Drawing.Point(84, 88);
+            this.dtpDueDay.Name = "dtpDueDay";
+            this.dtpDueDay.Size = new System.Drawing.Size(125, 26);
+            this.dtpDueDay.TabIndex = 0;
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.Location = new System.Drawing.Point(84, 38);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(125, 26);
+            this.dtpStart.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 19);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Start";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "DueDay";
+            // 
+            // timerNow
+            // 
+            this.timerNow.Enabled = true;
+            this.timerNow.Interval = 1000;
+            this.timerNow.Tick += new System.EventHandler(this.timerNow_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(655, 442);
-            this.Name = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1033, 221);
+            this.Controls.Add(this.grbSetting);
+            this.Controls.Add(this.grbStatus);
+            this.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmMain";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.grbStatus.ResumeLayout(false);
+            this.grbStatus.PerformLayout();
+            this.grbSetting.ResumeLayout(false);
+            this.grbSetting.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar prbDays;
+        private System.Windows.Forms.GroupBox grbStatus;
+        private System.Windows.Forms.Label lblMinutes;
+        private System.Windows.Forms.ProgressBar prbMinutes;
+        private System.Windows.Forms.Label lblHours;
+        private System.Windows.Forms.ProgressBar prbHours;
+        private System.Windows.Forms.Label lblDays;
+        private System.Windows.Forms.GroupBox grbSetting;
+        private System.Windows.Forms.DateTimePicker dtpDueDay;
+        private System.Windows.Forms.Button btnSetting;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.Timer timerNow;
     }
 }
 
--- DaysCountdown/frmMain.cs
+++ DaysCountdown/frmMain.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using Edward;
 
 namespace DaysCountdown
 {
@@ -14,5 +16,213 @@ namespace DaysCountdown
         {
             InitializeComponent();
         }
+
+        #region parameters
+        static string AppFolder = Application.StartupPath + @"DaysCountdown";
+        static string IniFilePath = AppFolder + @"\SysConfig.ini";
+        static string _DueDay = "";
+        static string _StartDay = "";
+
+        static int SETTINGWIDTH = 1039;
+        static int RUNWIDTH = 777;
+
+        static DateTime DueDay;
+        static DateTime StartDay;
+        int tempSec = 0;
+       
+
+        #endregion
+
+
+
+        #region checkFolder
+
+        /// <summary>
+        /// check folder & config file,if not exits,create 
+        /// </summary>
+        private void CheckFolderConfig()
+        {
+            if (!Directory.Exists(AppFolder))
+                Directory.CreateDirectory(AppFolder);
+
+            if (!File.Exists(IniFilePath))
+            {
+                IniFile.CreateIniFile(IniFilePath);
+                // write default config
+                IniFile.IniWriteValue("SysConfig", "StartDay", "", IniFilePath);
+                IniFile.IniWriteValue("SysConfig", "DueDay", "", IniFilePath);
+            }
+        }
+
+        #endregion
+
+        #region LoadConfig
+
+        private void LoadConfig()
+        {
+            _StartDay = IniFile.IniReadValue ("SysConfig", "StartDay", IniFilePath);
+            _DueDay = IniFile.IniReadValue("SysConfig", "DueDay", IniFilePath);
+
+            if (string.IsNullOrEmpty(_StartDay))
+            {
+                this.Width = SETTINGWIDTH;
+                MessageBox.Show("You don't set the START DAY,pls setting...", "DO NOT SET START DAY", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                dtpStart.Focus();
+                return;
+            }
+            else
+            {
+                this.Width = RUNWIDTH;
+
+                try
+                {
+                    StartDay  = Convert.ToDateTime(_StartDay );
+                }
+                catch (Exception ex)
+                {
+                    this.Width = SETTINGWIDTH;
+                    MessageBox.Show("Can't convert setting to DATTIME,pls check the setting...,details message:" + ex.Message, "CONVERT ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    dtpDueDay.Focus();
+                    return;
+                }
+            }
+
+
+
+            if (string.IsNullOrEmpty(_DueDay))
+            {
+                this.Width = SETTINGWIDTH;
+                MessageBox.Show("You don't set the DUE DAY,pls setting...", "DO NOT SET DUE DAY", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                dtpDueDay.Focus();
+            }
+            else
+            {
+                this.Width = RUNWIDTH;
+
+                try
+                {
+                    DueDay = Convert.ToDateTime(_DueDay);
+                }
+                catch (Exception ex)
+                {
+                    this.Width = SETTINGWIDTH;
+                    MessageBox.Show("Can't convert setting to DATTIME,pls check the setting...,details message:" + ex.Message,"CONVERT ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    dtpDueDay.Focus();
+                }
+            }
+        }
+
+        #endregion
+
+        
+
+
+
+        #region CalcDateDiff
+
+
+        /// <summary>
+        /// define datetime diff result type
+        /// </summary>
+        enum DateDiffResultType
+        {
+            Days,
+            Hours,
+            Minutes
+        }
+
+
+
+        /// <summary>
+        /// calculate dt1 subtract dt2 
+        /// </summary>
+        /// <param name="_dt1">dt1</param>
+        /// <param name="_dt2">dt2</param>
+        /// <param name="_resulttype">result type</param>
+        /// <returns></returns>
+        private int  CalcDateDiff(DateTime _dt1, DateTime _dt2,DateDiffResultType _resulttype)
+        {
+
+            TimeSpan ts1 = new TimeSpan(_dt1.Ticks);
+            TimeSpan ts2 = new TimeSpan(_dt2.Ticks);
+            TimeSpan ts = ts1.Subtract(ts2).Duration();
+
+            int    diff;
+
+
+            switch (_resulttype)
+            {
+                case DateDiffResultType.Days:
+                    diff = ts.Days;
+                    break;
+                case DateDiffResultType.Hours:
+                    diff = ts.Hours;
+                    break;
+                case DateDiffResultType.Minutes:
+                    diff = ts.Minutes;
+                    break;
+                default:
+                    diff = ts.Seconds;
+                    break;
+            }
+            return diff;
+
+        }
+
+
+        private void SetProgress()
+        {
+
+
+            prbDays.Maximum = Convert.ToInt32(CalcDateDiff(DueDay, StartDay, DateDiffResultType.Days));
+            prbHours.Maximum = 24;
+            prbMinutes.Maximum = 60;
+           // prbHours.Maximum = Convert.ToInt32(CalcDateDiff(DueDay, StartDay, DateDiffResultType.Hours));
+           // prbMinutes.Maximum = Convert.ToInt32(CalcDateDiff(DueDay, StartDay, DateDiffResultType.Minutes));
+
+            prbDays.Value = Convert.ToInt32(CalcDateDiff(DateTime.Now , StartDay, DateDiffResultType.Days));
+            prbHours.Value = Convert.ToInt32(CalcDateDiff(DateTime.Now, StartDay, DateDiffResultType.Hours));
+            prbMinutes.Value  = Convert.ToInt32(CalcDateDiff(DateTime.Now, StartDay, DateDiffResultType.Minutes));
+
+        }
+
+
+
+
+        #endregion
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+            CheckFolderConfig();
+            LoadConfig();
+            SetProgress();
+        }
+
+        private void btnSetting_Click(object sender, EventArgs e)
+        {
+            _StartDay = dtpStart.Value.ToString("yyyy-MM-dd");
+            IniFile.IniWriteValue("SysConfig", "StartDay", _StartDay, IniFilePath);
+            _DueDay = dtpDueDay.Value.ToString("yyyy-MM-dd");
+            IniFile.IniWriteValue("SysConfig", "DueDay", _DueDay, IniFilePath);
+            MessageBox.Show("Setting OK,the proramm will RESTART", "SETTING & RESTART", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Application.Restart();
+        }
+
+        private void timerNow_Tick(object sender, EventArgs e)
+        {
+            grbStatus.Text = "Countdown Status: " + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss") + ",Due Day:" + _DueDay;
+            if (DateTime.Now.Second == 0)
+            {
+                prbDays.Value = Convert.ToInt32(CalcDateDiff(DateTime.Now, StartDay, DateDiffResultType.Days));
+                prbHours.Value = Convert.ToInt32(CalcDateDiff(DateTime.Now, StartDay, DateDiffResultType.Hours));
+                prbMinutes.Value = Convert.ToInt32(CalcDateDiff(DateTime.Now, StartDay, DateDiffResultType.Minutes));
+            }
+
+       
+        }
+
+
+
     }
 }
